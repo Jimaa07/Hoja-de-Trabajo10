05b774b3f48863132717edce274a0b6291899a9c
Jimaa07 <jimafotos247@gmail.com>
2026-05-19 21:20:53 -0600

    menu

---
+++ .metals/.reports/metals-full/2026-05-19/r_empty-definition_21-10-49-051.md
@@ -0,0 +1,75 @@
+error id: file:///C:/Users/Jair%20Morales/OneDrive%20-%20UVG/Escritorio/Progra2/Hoja-de-Trabajo10/src/Sistema.java:_empty_/Sistema#Sistema#mostrarMenu#
+file:///C:/Users/Jair%20Morales/OneDrive%20-%20UVG/Escritorio/Progra2/Hoja-de-Trabajo10/src/Sistema.java
+empty definition using pc, found symbol in pc: _empty_/Sistema#Sistema#mostrarMenu#
+empty definition using semanticdb
+empty definition using fallback
+non-local guesses:
+
+offset: 234
+uri: file:///C:/Users/Jair%20Morales/OneDrive%20-%20UVG/Escritorio/Progra2/Hoja-de-Trabajo10/src/Sistema.java
+text:
+```scala
+package src;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Sistema {
+
+    private Grafo grafo;
+    public Sistema() {
+        grafo = new Grafo(30);
+    }
+public void iniciar(){
+
+    cargarArchivo();
+    @@mostrarMenu();
+    }
+    int[][] distancias = grafo.floydWarshall();
+
+    mostrarDistancias(distancias);
+
+    System.out.println("\nCENTRO DEL GRAFO:");
+    System.out.println(grafo.centroGrafo());
+    }
+
+
+    private void cargarArchivo() {
+        try {
+            File archivo = new File("guategrafo.txt");
+            Scanner sc = new Scanner(archivo);
+            while (sc.hasNext()) {
+                String origen = sc.next();
+                String destino = sc.next();
+                int distancia = sc.nextInt();
+                grafo.agregarArco(origen, destino,distancia);
+            }
+
+            sc.close();
+
+        } catch (Exception e) {
+            System.out.println("Error al leer archivo");
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    private void mostrarDistancias(int[][] distancias) {
+    System.out.println("MATRIZ DE DISTANCIAS MINIMAS:\n");
+    for (int i = 0; i < distancias.length; i++) {
+        for (int j = 0; j < distancias.length; j++) {
+            if (distancias[i][j] == 999999) {
+                System.out.print("INF\t");
+            } else {
+                System.out.print(distancias[i][j] + "\t");
+            }
+        }
+        System.out.println();
+    }
+}
+}
+```
+
+
+#### Short summary: 
+
+empty definition using pc, found symbol in pc: _empty_/Sistema#Sistema#mostrarMenu#
--- src/Sistema.java
+++ src/Sistema.java
@@ -6,52 +6,134 @@ import java.util.Scanner;
 public class Sistema {
 
     private Grafo grafo;
-    public Sistema() {
-        grafo = new Grafo(30);
-    }
-public void iniciar(){
-
-    cargarArchivo();
-
-    int[][] distancias = grafo.floydWarshall();
-
-    mostrarDistancias(distancias);
-
-    System.out.println("\nCENTRO DEL GRAFO:");
-    System.out.println(grafo.centroGrafo());
+    public Sistema(){
+        grafo = new Grafo(100);
     }
 
+    public void iniciar(){
+        cargarArchivo();
+        mostrarMenu();
+    }
 
-    private void cargarArchivo() {
-        try {
+    private void cargarArchivo(){
+        try{
+
             File archivo = new File("guategrafo.txt");
             Scanner sc = new Scanner(archivo);
-            while (sc.hasNext()) {
+
+            while(sc.hasNext()){
+
                 String origen = sc.next();
                 String destino = sc.next();
                 int distancia = sc.nextInt();
-                grafo.agregarArco(origen, destino,distancia);
+
+                grafo.agregarArco(origen, destino, distancia);
             }
 
             sc.close();
 
-        } catch (Exception e) {
+        }catch(Exception e){
+
             System.out.println("Error al leer archivo");
             System.out.println(e.getMessage());
         }
     }
-    
-    private void mostrarDistancias(int[][] distancias) {
-    System.out.println("MATRIZ DE DISTANCIAS MINIMAS:\n");
-    for (int i = 0; i < distancias.length; i++) {
-        for (int j = 0; j < distancias.length; j++) {
-            if (distancias[i][j] == 999999) {
-                System.out.print("INF\t");
-            } else {
-                System.out.print(distancias[i][j] + "\t");
+
+    private void mostrarMenu(){
+
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        while(opcion != 4){
+
+            System.out.println("\n----- MENU -----");
+            System.out.println("1. Ruta minima");
+            System.out.println("2. Centro del grafo");
+            System.out.println("3. Modificar grafo");
+            System.out.println("4. Salir");
+            System.out.print("Opcion: ");
+
+            opcion = sc.nextInt();
+
+            switch(opcion){
+
+                case 1:
+                    consultarRuta(sc);
+                    break;
+
+                case 2:
+                    mostrarCentro();
+                    break;
+
+                case 3:
+                    modificarGrafo(sc);
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida");
             }
         }
-        System.out.println();
     }
-}
+
+    private void consultarRuta(Scanner sc){
+
+        sc.nextLine();
+
+        System.out.print("Ciudad origen: ");
+        String origen = sc.nextLine();
+
+        System.out.print("Ciudad destino: ");
+        String destino = sc.nextLine();
+
+        System.out.println("\nRuta minima:");
+
+        System.out.println(
+                grafo.obtenerRuta(origen, destino)
+        );
+    }
+
+    private void mostrarCentro(){
+
+        System.out.println("\nCENTRO DEL GRAFO:");
+        System.out.println(grafo.centroGrafo());
+    }
+
+    private void modificarGrafo(Scanner sc){
+
+        System.out.println("\n1. Eliminar conexion");
+        System.out.println("2. Agregar conexion");
+
+        int opcion = sc.nextInt();
+
+        sc.nextLine();
+
+        if(opcion == 1){
+
+            System.out.print("Origen: ");
+            String origen = sc.nextLine();
+
+            System.out.print("Destino: ");
+            String destino = sc.nextLine();
+
+            grafo.eliminarArco(origen, destino);
+            System.out.println("Conexion eliminada");
+
+        }else if(opcion == 2){
+
+            System.out.print("Origen: ");
+            String origen = sc.nextLine();
+
+            System.out.print("Destino: ");
+            String destino = sc.nextLine();
+
+            System.out.print("Distancia: ");
+            int distancia = sc.nextInt();
+
+            grafo.agregarArco(origen, destino, distancia);
+            System.out.println("Conexion agregada");
+        }
+    }
 }
